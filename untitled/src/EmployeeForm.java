a24c41fc4f4edd6af3fcceb3cb9e7e696f81b372
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-05-18 21:49:30 +0530

    DB connection

--- untitled/src/EmployeeForm.java
+++ untitled/src/EmployeeForm.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class EmployeeForm {
     private JTextField txtname;
@@ -9,4 +15,38 @@ public class EmployeeForm {
     private JButton updateButton;
     private JButton saveButton;
     private JTable table1;
+    private JPanel main;
+    private JTextField textField1;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("EmployeeForm");
+        frame.setContentPane(new EmployeeForm().main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    Connection con;
+    PreparedStatement pst;
+    public void connect(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/company","root","");
+            System.out.println("Success..");
+        } catch(ClassNotFoundException ex){
+            ex.printStackTrace();
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public EmployeeForm() {
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
