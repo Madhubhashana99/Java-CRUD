35a7e158e30898dae39e0a6db344f54d8216ba5b
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-05-18 21:03:08 +0530

    create UI

---
+++ untitled/src/EmployeeForm.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class EmployeeForm {
+    private JTextField txtname;
+    private JTextField txtsalary;
+    private JTextField txtmobile;
+    private JButton deleteButton;
+    private JButton searchButton;
+    private JButton updateButton;
+    private JButton saveButton;
+    private JTable table1;
+}
